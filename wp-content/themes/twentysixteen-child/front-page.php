23f6901e89404343dd7ecc963e421fe91a4774a5
MichaelSM <msheilsmcnamee@gmail.com>
2018-03-05 17:43:45 +0000

    updates made to design and structure

--- wp-content/themes/twentysixteen-child/front-page.php
+++ wp-content/themes/twentysixteen-child/front-page.php
@@ -27,18 +27,42 @@ get_header(); ?>
  */
 
 get_header(); ?>
-
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-        <div class="page-header">
-            <h1>Welcome to the Back Pain Clinic</h1>
+        <?php while ( have_posts() ) : the_post(); 
+            $banner_image = get_field('banner_image');
+            $profile_image = get_field('profile_image');
+            $accreditation_header = get_field('accreditation_header');
+            $accreditation_content = get_field('accreditation_content');
+            $size ='full';
+        ?>
+        
+        <div>
+            <p class="banner-image"><?php if($banner_image) { 
+                echo wp_get_attachment_image($banner_image, $size);
+                } ?></p>
         </div>
         
-        <div class="page-content">
-            <?php the_content(); ?>
-        </div>    
+            <div class="page-header">
+            
+                <h1>Welcome to the Back Pain Clinic</h1>
+            </div>        
+            <div class="page-content">
+				<?php the_content(); ?>
+                <p class="profile-image"><?php if($profile_image) { 
+                echo wp_get_attachment_image($profile_image, $size);
+                } ?></p>
+            </div>    
+        
+            <div class="accreditation-content">
+                <h2 class="header-accreditation"><?php echo $accreditation_header; ?></h2>
+                <p class="main-accreditation"><?php echo $accreditation_content; ?>
+                </p> 
+            </div>
 
-	</main><!-- .site-main -->
+    </main><!-- .site-main -->
 </div><!-- .content-area -->
+
+		<?php endwhile; // end of the loop. ?>
 
 <?php get_footer(); ?>
