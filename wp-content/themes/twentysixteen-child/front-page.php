6ee1ebb6786effcefa79ea2df3126531a77acceb
MichaelSM <msheilsmcnamee@gmail.com>
2018-03-12 09:46:32 +0000

    changes on all three pages - before making changes in the main theme to get rid of the header

--- wp-content/themes/twentysixteen-child/front-page.php
+++ wp-content/themes/twentysixteen-child/front-page.php
@@ -13,25 +13,13 @@
 
 get_header(); ?>
 
-<?php
-/**
- * The template for displaying pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
- */
-
-get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
         <?php while ( have_posts() ) : the_post(); 
             $banner_image = get_field('banner_image');
             $profile_image = get_field('profile_image');
+            $services_header = get_field('services_header');
+            $services_content = get_field('services_content');
             $accreditation_header = get_field('accreditation_header');
             $accreditation_content = get_field('accreditation_content');
             $size ='full';
@@ -47,12 +35,20 @@ get_header(); ?>
             
                 <h1>Welcome to the Back Pain Clinic</h1>
             </div>        
+            
             <div class="page-content">
 				<?php the_content(); ?>
                 <p class="profile-image"><?php if($profile_image) { 
                 echo wp_get_attachment_image($profile_image, $size);
                 } ?></p>
             </div>    
+            
+            <div class="services-header">
+                <h2 class="services-header"><?php echo $services_header; ?>
+                </h2>
+                <p class="services-content"><?php echo $services_content; ?>
+                </p>
+            </div>
         
             <div class="accreditation-content">
                 <h2 class="header-accreditation"><?php echo $accreditation_header; ?></h2>
@@ -63,6 +59,7 @@ get_header(); ?>
     </main><!-- .site-main -->
 </div><!-- .content-area -->
 
-		<?php endwhile; // end of the loop. ?>
-
 <?php get_footer(); ?>
+
+<?php endwhile; // end of the loop. ?>
+
--- wp-content/themes/twentysixteen-child/page-contact_us.php
+++ wp-content/themes/twentysixteen-child/page-contact_us.php
@@ -1,85 +1,34 @@
-<?php
-/**
- * Template for: The About custom page
- *
- * @package WordPress
- * @subpackage Accelerate Marketing
- * @since Accelerate Marketing 2.0
- */
-get_header(); ?>
-<div id="primary">
-    <div class="main-content" role="main">
+<?php /* Template Name: page-contact_us.php */ ?>
+
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
         <?php while ( have_posts() ) : the_post(); 
-            $banner_text = get_field('banner_text');
-            $header_one = get_field('header_one');        
-            $header_two = get_field('header_two');
-            $header_three = get_field('header_three');
-            $header_four = get_field('header_four');
-            $content_one = get_field('content_one');
-            $content_two = get_field('content_two');
-            $content_three = get_field('content_three');
-            $content_four = get_field('content_four');
-            $image_one = get_field('image_one');
-            $image_two = get_field('image_two');
-            $image_three = get_field('image_three');
-            $image_four = get_field('image_four');
-            $contact_text = get_field('contact_text');
-            $size = 'full';
         ?>
-        
-        <div class="the_banner">
-        <h3><?php echo $banner_text; ?></h3>        
-        </div>
-        <body>
-        <div class="the_content">
-            <?php the_content(); ?>
-        
-        </div>
-        
-        
-    <article class="examples">
-        
-        <div class="section one">
-            <h2 class="header_r"><?php echo $header_one; ?></h2>
-            <p class="description_r"><?php echo $content_one; ?></p> 
-            <p class="img_l"><?php if($image_one) { 
-            echo wp_get_attachment_image($image_one, $size);
-            } ?></p>
-        
-        </div>
-              
-        <div class="section two">
-            <h2 class="header_l"><?php echo $header_two; ?></h2>
-            <p class="description_l"><?php echo $content_two; ?></p>
-            <p class="img_r"><?php if($image_two){
-            echo wp_get_attachment_image ($image_two, $size);
-            } ?></p>
-        
-        </div>
-        
-        <div class="section three">
-            <h2 class="header_r"><?php echo $header_three; ?></h2>
-            <p class="description_r"><?php echo $content_three; ?></p>
-            <p class="img_l"><?php if($image_three){
-            echo wp_get_attachment_image ($image_three, $size);
-            } ?></p>
-        
-        </div>
-        
-        <div class="section four">
-            <h2 class="header_l"><?php echo $header_four; ?></h2>
-            <p class="description_l"><?php echo $content_four; ?></p>
-            <p class="img_r"><?php if($image_four){
-            echo wp_get_attachment_image ($image_four, $size);
-            } ?></p>
-        </div>
-    </article>
-    </body>
-		<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
-    </div><!-- #primary -->
-    <div id="contact">
-        <h3><?php echo $contact_text; ?></h3>
-        <a class="button" href="<?php echo site_url('/contact-us/') ?>">Contact Us</a>
+    
+    <div class="page-header">
+
+        <h1><?php the_title(); ?></h1>
+    
     </div>
+          
+    <div class="page-content">
+    
+        <?php the_content(); ?>
+    
+    </div>
+    
+        
+	</main><!-- .site-main -->
+
+
+</div><!-- .content-area -->
+
+
+
+<?php endwhile; // end of the loop. ?>
+
 <?php get_footer(); ?>
+
+
