34f12f335f07ac990805d2ded5a0211cf1c81217
MichaelSM <msheilsmcnamee@gmail.com>
2018-04-02 19:48:09 +0100

    made changes to style of contact us page

--- wp-content/themes/twentysixteen-child/page-contact_us.php
+++ wp-content/themes/twentysixteen-child/page-contact_us.php
@@ -13,14 +13,18 @@
     
     </div>
           
-    <div class="page-content">
+    <div class="contact-body">
+        <div class="contact-content">
     
-        <?php the_content(); ?>
-    
-    </div>
-    
+            <?php the_content(); ?>
         
-	</main><!-- .site-main -->
+        </div>
+    `   <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1144.4392205124648!2d-7.3207538618711885!3d54.99276524053241!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x485fe19d05256b99%3A0x1c4c5efcc9f4def0!2sBack+Pain+Clinic!5e0!3m2!1sen!2suk!4v1522693592766" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </div>    
+	
+</main><!-- .site-main -->
 
 
 </div><!-- .content-area -->
@@ -30,5 +34,3 @@
 <?php endwhile; // end of the loop. ?>
 
 <?php get_footer(); ?>
-
-
