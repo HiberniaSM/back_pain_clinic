1c9a8acb6da4652c107883757f406b991308ab99
MichaelSM <msheilsmcnamee@gmail.com>
2018-04-02 20:19:28 +0100

    updated contact us and about us pages

--- wp-content/themes/twentysixteen-child/page-about.php
+++ wp-content/themes/twentysixteen-child/page-about.php
@@ -5,18 +5,29 @@
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
         <?php while ( have_posts() ) : the_post(); 
+            $about_image = get_field('about_image');
+            $size ='full';
         ?>
-    
+        
     <div class="page-header">
         <h1><?php the_title(); ?></h1>
     </div>
-          
-    <div class="page-content">
     
-        <?php the_content(); ?>
+    <div class="about-main">
+        
+        <div class="about-content">
     
+            <?php the_content(); ?>
+    
+        </div>
+        
+        <div class="about-image"> 
+            <?php if($about_image) { 
+                echo wp_get_attachment_image($about_image, $size);
+                } ?>
+        </div>
+        
     </div>
-    
         
 	</main><!-- .site-main -->
 
